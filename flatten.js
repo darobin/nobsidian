001be593ed0de1dab76edb0bbbb8c3d56399cf35
Robin Berjon <robin.berjon@nytimes.com>
2023-05-08 09:20:49 -0400

    'first phase'

--- flatten.js
+++ flatten.js
@@ -2,7 +2,7 @@
 import recursiveReaddirFiles from 'recursive-readdir-files';
 
 const baseDir = '/Users/robin/Documents/Documents - Dawon/Obsidian/Everything/';
-const files = (await recursiveReaddirFiles(baseDir))
+const files = (await recursiveReaddirFiles(baseDir, { ignored: /\.obsidian|\.DS_Store/ }))
   .map(({ path, name, ext }) => ({
     name,
     localName: path.replace(baseDir, ''),
@@ -10,4 +10,16 @@ const files = (await recursiveReaddirFiles(baseDir))
     ext,
   }))
 ;
-console.warn(files);
+const fileMap = {};
+files.forEach(({ name, localName }) => {
+  if (!fileMap[name]) fileMap[name] = [];
+  fileMap[name].push(localName);
+});
+let count = 0;
+Object.entries(fileMap).forEach(([k, v]) => {
+  if (v.length > 1) {
+    console.log(`${k}: ${v.join('|')}`);
+    count++;
+  }
+});
+console.warn(`Total: ${count}`);
